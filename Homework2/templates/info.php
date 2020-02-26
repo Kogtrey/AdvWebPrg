7bd406853a98a7e34c7baa31da735369db044fd5
Kody <kodyneill@gmail.com>
2020-02-26 09:38:48 -0700

    17

--- Homework2/templates/info.php
+++ Homework2/templates/info.php
@@ -3,10 +3,11 @@
   <head>
     <meta charset="utf-8">
     <title></title>
+    <link rel="stylesheet" href="/css/info.css">
   </head>
   <body>
     <div class="paper">
-      
+      <h4>Paper Here</h4>
     </div>
   </body>
 </html>
